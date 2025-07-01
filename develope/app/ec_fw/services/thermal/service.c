6e9f8e55ec1f22b89d1e2bfcdc40e631381de47c
MCHP-Andy <andy.chang@microchip.com>
2025-07-02 00:56:41 +0800

    test(thermal): test service for tmp451, fan and tach

--- develope/app/ec_fw/services/thermal/service.c
+++ develope/app/ec_fw/services/thermal/service.c
@@ -2,20 +2,29 @@
  * @Author: andy.chang 
  * @Date: 2025-07-01 02:46:45 
  * @Last Modified by: andy.chang
- * @Last Modified time: 2025-07-01 11:02:40
+ * @Last Modified time: 2025-07-02 00:55:52
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 #include <interfaces/thermal.h>
+#include <drivers/tmp451.h>
 
 LOG_MODULE_REGISTER(thermal, LOG_LEVEL_INF);
 
 #define STACKSIZE 1024
 #define PRIORITY 7
 
+static const struct i2c_dt_spec tmp_devs[] = {
+    I2C_DT_SPEC_GET(DT_NODELABEL(tmp451_0)),
+    I2C_DT_SPEC_GET(DT_NODELABEL(tmp451_1)),
+};
+
 static void service(void) {
+
+    k_sleep(K_MSEC(10000));
+    
     LOG_INF("Thermal service started");
 
     // Initialize thermal sensors, fans, etc.
@@ -24,6 +33,9 @@ static void service(void) {
     //     LOG_ERR("Failed to initialize thermal service: %d", ret);
     //     return;
     // }
+    for (size_t i = 0; i < ARRAY_SIZE(tmp_devs); i++) {
+        tmp451_init(&tmp_devs[i]);
+    }
 
     while (1) {
         // Monitor temperature and control fans
@@ -32,7 +44,38 @@ static void service(void) {
         //     LOG_ERR("Thermal monitoring failed: %d", ret);
         // }
 
-        k_sleep(K_MSEC(1000)); // Sleep for 1 second before next iteration
+        for (size_t i = 0; i < ARRAY_SIZE(tmp_devs); i++) {
+            uint16_t tmp = 0;
+            tmp451_read(&tmp_devs[i], 0, &tmp);
+            LOG_INF("TMP451[%d] Remote Temperature: %d.%d C", i, tmp / 100,
+                    tmp % 100);
+        }
+
+        static uint8_t cnt = 0;
+        for (size_t i = 0; i < 2; i++) {
+            int ret = 0;
+            uint16_t rpm = 0;
+
+            ret = app_fan_set_speed(i, cnt);
+            if (ret < 0) {
+                LOG_ERR("Failed to set fan%d speed: %d", i, ret);
+            } else {
+                LOG_INF("Fan%d speed set to %d%%", i, cnt);
+            }
+
+            ret = app_fan_get_rpm(i, &rpm);
+            if (ret < 0) {
+                LOG_ERR("Failed to get fan%d RPM: %d", i, ret);
+            } else {
+                LOG_INF("Fan%d RPM is %d", i, rpm);
+            }
+        }
+        cnt += 10;
+        if (cnt > 100) {
+            cnt = 0;
+        }
+
+        k_sleep(K_MSEC(5000)); // Sleep for 5 second before next iteration
     }
 	
 }
