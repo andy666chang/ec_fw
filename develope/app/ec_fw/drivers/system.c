ad986f5843b8d8c8bddbb8f65ad2958901294f1b
MCHP-Andy <andy.chang@microchip.com>
2025-07-13 02:55:38 +0800

    refactor(system): refine system event

---
+++ develope/app/ec_fw/drivers/system.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: andy.chang 
+ * @Date: 2025-07-13 02:48:49 
+ * @Last Modified by: andy.chang
+ * @Last Modified time: 2025-07-13 02:49:33
+ */
+
+#include <zephyr/kernel.h>
+
+#include <interface/system.h>
+
+APP_EVENT_TYPE_DEFINE(system_event);
